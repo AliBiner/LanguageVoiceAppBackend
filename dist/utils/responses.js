270bd8e3fd8f7cf8e656642a780ec171caf27a15
Ali Biner <alibiner.github@gmail.com>
2024-04-07 08:35:55 +0300

    test

--- dist/utils/responses.js
+++ dist/utils/responses.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 class CustomResponse {
-    constructor({ data = null, message = null }) {
+    constructor({ data = null, message = null, }) {
         this.data = data;
         this.message = message;
     }
@@ -10,61 +10,55 @@ class CustomResponse {
         return response.status(200).json({
             success: true,
             data: this.data,
-            message: (_a = this.message) !== null && _a !== void 0 ? _a : "Success"
+            message: (_a = this.message) !== null && _a !== void 0 ? _a : "Success",
         });
     }
     created(response) {
         var _a;
-        return response.status(201)
-            .json({
+        return response.status(201).json({
             success: true,
             data: this.data,
-            message: (_a = this.message) !== null && _a !== void 0 ? _a : "Created"
+            message: (_a = this.message) !== null && _a !== void 0 ? _a : "Created",
         });
     }
     error_500(response) {
         var _a;
-        return response.status(500)
-            .json({
+        return response.status(500).json({
             success: false,
             data: this.data,
-            message: (_a = this.message) !== null && _a !== void 0 ? _a : "Failed! Server Error!"
+            message: (_a = this.message) !== null && _a !== void 0 ? _a : "Failed! Server Error!",
         });
     }
     error_400(response) {
         var _a;
-        return response.status(400)
-            .json({
+        return response.status(400).json({
             success: false,
             data: this.data,
-            message: (_a = this.message) !== null && _a !== void 0 ? _a : "Failed! Bad Request!"
+            message: (_a = this.message) !== null && _a !== void 0 ? _a : "Failed! Bad Request!",
         });
     }
     error_401(response) {
         var _a;
-        return response.status(401)
-            .json({
+        return response.status(401).json({
             success: false,
             data: this.data,
-            message: (_a = this.message) !== null && _a !== void 0 ? _a : "Failed! Unauthorized!"
+            message: (_a = this.message) !== null && _a !== void 0 ? _a : "Failed! Unauthorized!",
         });
     }
     error_404(response) {
         var _a;
-        return response.status(404)
-            .json({
+        return response.status(404).json({
             success: false,
             data: this.data,
-            message: (_a = this.message) !== null && _a !== void 0 ? _a : "Failed! Not Found!"
+            message: (_a = this.message) !== null && _a !== void 0 ? _a : "Failed! Not Found!",
         });
     }
     error_429(response) {
         var _a;
-        return response.status(429)
-            .json({
+        return response.status(429).json({
             success: false,
             data: this.data,
-            message: (_a = this.message) !== null && _a !== void 0 ? _a : "Failed! Over Request!"
+            message: (_a = this.message) !== null && _a !== void 0 ? _a : "Failed! Over Request!",
         });
     }
 }
--- netlify/functions/me.ts
+++ netlify/functions/me.ts
@@ -1,9 +1,8 @@
 import { Handler } from "serverless-http";
 import { HandlerEvent, HandlerContext } from "@netlify/functions";
-import CustomResponse from "../../src/utils/responses";
 
-const meHandler: Handler = async (event: any, context: any) => {
-  return new Response("test");
+const meHandler: Handler = async (event, context) => {
+  return new Response(typeof event);
 };
 
 export default meHandler;
