2677c6c7081048d025f1788ff2c1fdecbd64bdbb
Ali Biner <alibiner.github@gmail.com>
2024-04-24 16:36:36 +0300

    upload path of worker

--- dist/controllers/auth_controller.js
+++ dist/controllers/auth_controller.js
@@ -49,7 +49,7 @@ function me(request, response) {
 exports.me = me;
 function createWorker() {
     return new Promise((resolve, reject) => {
-        const worker = new worker_threads_1.Worker("../../../../../../../home/ali/Desktop/TsProject/src/threads/meWorker.js");
+        const worker = new worker_threads_1.Worker("../threads/meWorker.js");
         worker.postMessage("Hello, World!!");
         worker.on("message", (code) => {
             resolve(code);
