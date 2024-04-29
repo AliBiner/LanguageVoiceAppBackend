d7690469e14e85be40dfb4843d2315b1c75ce52f
Ali Biner <alibiner.github@gmail.com>
2024-04-29 13:19:54 +0300

    add varaible type

--- src/controllers/meWorker.ts
+++ src/controllers/meWorker.ts
@@ -1,6 +1,6 @@
 const { parentPort } = require("worker_threads");
 const bcrypt = require("bcrypt");
-parentPort.on("message", async (value) => {
+parentPort.on("message", async (value: any) => {
   const pass = await bcrypt.hash(value, 1);
   parentPort.postMessage(pass);
   parentPort.close;
